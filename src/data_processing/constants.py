df1594709e40c27cb17c267a9d3c8cafeeed38ba
jirhiker <jirhiker@localhost>
2011-12-12 11:22:47 -0700

    added arar report gen added arar calculations

--- src/data_processing/constants.py
+++ src/data_processing/constants.py
@@ -23,20 +23,52 @@ limitations under the License.
 ##=============enthought library imports=======================
 #
 ##=============standard library imports ========================
-#from uncertainties import ufloat
+from uncertainties import ufloat
 #
 ##=============local library imports  ==========================
-##decay constants
+
+
 #lambda_epsilon = ufloat((5.81e-11,
 #                                    1.7e-12))
 #lambda_beta = ufloat((4.962e-10,
 #                                 8.6e-12))
+lambda_epsilon = ufloat((5.81e-11,
+                                    0))
+lambda_beta = ufloat((4.962e-10,
+                                 0))
+lambdak = lambda_epsilon + lambda_beta
+
+lambda_37 = ufloat((0.01975, 0)) #per day
+lambda_39 = ufloat((7.068000e-6, 0))  #per day
+lambda_cl36 = ufloat((6.308000e-9, 0))  #per day
+
+#atmospheric ratios
+#atm4036 = ufloat((295.5, 0.5))
+#atm4038 = ufloat((1575, 2))
+atm4036 = ufloat((295.5, 0.5))
+atm4038 = ufloat((1575, 2))
+atm3638 = atm4038 / atm4036
+atm3836 = atm4036 / atm4038
+
+
+###decay constants
+#lambda_epsilon = 5.81e-11
+#lambda_epsilon_er = 1.7e-12
+##                                    1.7e-12))
+#lambda_beta = 4.962e-10
+#lambda_beta_er = 8.6e-12
 #lambdak = lambda_epsilon + lambda_beta
 #
-#lambda_37 = ufloat((0.01975, 0)) #per day
-#lambda_39 = ufloat((7.068000e-6, 0))  #per day
-#lambda_cl36 = ufloat((6.308000e-9, 0))  #per day
-##atmospheric ratios
-#atm40_36 = ufloat((295.5, 0.5))
-#atm40_38 = ufloat((1575, 2))
-#atm36_38 = atm40_38 / atm40_36
+#
+#lambda_37 = 0.01975 #per day
+#lambda_39 = 7.068000e-6  #per day
+#
+#lambda_cl36 = 6.308000e-9  #per day
+###atmospheric ratios
+#atm4036 = 295.5
+#atm4036_er = 0.5
+#atm4038 = 1575
+#atm4038_er = 2
+##atm3638 = atm4038 / atm4036
+##atm3836 = atm4038 / atm4036
+#atm3836 = atm4036 / atm4038
--- src/report/report.py
+++ src/report/report.py
@@ -19,14 +19,16 @@ from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import inch
 
-#from reportlab.lib import colors
+from reportlab.lib import colors
+from src.report.autoupdate_parser import AutoupdateParser
 
-PAGE_HEIGHT = defaultPageSize[1]; PAGE_WIDTH = defaultPageSize[0]
+PAGE_HEIGHT = defaultPageSize[1] 
+PAGE_WIDTH = defaultPageSize[0]
 styles = getSampleStyleSheet()
 
 pageinfo = 'report'
 
-from reportlab.platypus.flowables import Flowable
+from reportlab.platypus.flowables import Flowable, DocIf
 class FlowableGraph(Flowable):
     def __init__(self, g, xoffset=0, size=None, fillcolor='red', strokcolor='black'):
         self.graph = g
@@ -44,6 +46,9 @@ class FlowableGraph(Flowable):
         self.graph.render_to_pdf(canvas=canvas, dest_box=(0, 0, self.size / inch, self.size / inch))
 
 
+        
+    
+    
 class Report:
     report_text = []
     def add_heading(self, h, i):
@@ -65,7 +70,8 @@ class Report:
     def add_graph(self, g):
         fg = FlowableGraph(g)
         self.report_text.append(fg)
-    def _paragraph(self, t, s):
+    
+    def _new_paragraph(self, t, s='Normal'):
         style = styles[s]
         p = Paragraph(t, style)
         return p
@@ -75,14 +81,19 @@ class Report:
         canvas.setFont('Helvetica', 10)
         canvas.drawCentredString(PAGE_WIDTH / 2.0, 0.5 * inch, 'page %d ' % doc.page)
         canvas.restoreState()
+    
+    def _plusminus_sigma(self, n=1):
+        s = unicode('\xb1{}'.format(n)) + unicode('\x73', encoding='Symbol')
+        return s
+    
     def create_cover_page(self, canvas, doc):
         #DRAW THE COVER PAGE
         #THIS SHOULD PROBABY EITHER NOT DO ANYTHING OR PRETTY PRINT PROJECT AND METADATA
         #PRINTS BRANDING INFO
 #        nmgrllogo=Image(
 #                        '/Users/Ross/ARLAB/images/NMGRLlogo.gif')
-        imp = '/Users/Ross/ARLAB/images/NMGRLlogo.gif'
-        canvas.drawInlineImage(imp, PAGE_WIDTH / 4.0, PAGE_HEIGHT / 2.0)
+#        imp = '/Users/Ross/ARLAB/images/NMGRLlogo.gif'
+#        canvas.drawInlineImage(imp, PAGE_WIDTH / 4.0, PAGE_HEIGHT / 2.0)
 
 
         author = 'Jake Ross'
@@ -94,54 +105,199 @@ class Report:
         canvas.drawCentredString(PAGE_WIDTH / 2.0, PAGE_HEIGHT / 2.0 - 1.8 * inch, nmt)
 
         import time
-        format = '%B %d,%Y'
-        date = time.strftime(format, time.gmtime())
+        fmt = '%B %d,%Y'
+        date = time.strftime(fmt, time.gmtime())
         canvas.drawCentredString(PAGE_WIDTH / 2.0, PAGE_HEIGHT / 2.0 - 2.1 * inch, date)
 
         self._draw_footer(canvas, doc)
         #canvas.setFont('Times-Roman',9) 
         #canvas.drawString(inch, 0.75 * inch, "First Page / %s" % pageinfo) 
     def create_report_page(self, canvas, doc):
-
-
         self._draw_footer(canvas, doc)
+        
+    def _build_spectrum_table(self, tablen, samples):
+        ar4039 = self._new_paragraph('<font size=9><super>40</super>Ar/<super>39</super>Ar</font>')
+        ar3739 = self._new_paragraph('<font size=9><super>37</super>Ar/<super>39</super>Ar</font>')
+        ar3639 = self._new_paragraph('<font size=9><super>36</super>Ar/<super>39</super>Ar</font>')
+        ar39k = self._new_paragraph('<font size=9><super>39</super>Ar<sub>K</sub></font>')
+        ar3639_mult = self._new_paragraph('<font size=7>(x 10<super>-3</super>)</font>')
+        ar39k_mult = self._new_paragraph('<font size=7>(x 10<super>-15</super> mol)</font>')
+        rad = self._new_paragraph('<font size=9><super>40</super>Ar*</font>')
+        sigma = self._new_paragraph(self._plusminus_sigma())
+        degrees = self._new_paragraph(unicode('(\xa1C)'))
+        ar39 = self._new_paragraph('<font size=9><super>39</super>Ar</font>')
+        
+        title = self._new_paragraph('<font size=10 name="Helvetica-Bold">Table {}. <super>40</super>Ar/<super>39</super>Ar analytical data.</font>'.format(tablen))
+        trows = [[title, ],
+                 [],
+                 ['', 'ID', 'Temp', ar4039 , ar3739, ar3639, ar39k, 'K/Ca', rad, ar39, 'Age', sigma],
+                 ['', '', degrees, '', '', ar3639_mult, ar39k_mult, '', '(%)', '(%)', '(Ma)', '(Ma)'],
+                 [],
+#                 [info_line]
+                 ]
+        
+        self.int_plat_age_rowids = []
+        self.sample_rowids = []
+        self.sample_ends = []
+        for s in samples:
+            self.sample_rowids.append(len(trows))
+            self._build_sample_rows(trows, s)
+            self.sample_ends.append(len(trows))
+            
+        ta = Table(trows)
+        self._set_column_widths(ta)
+        self._set_row_heights(ta)
+        tblstyle = self._build_spectrum_table_style()
+        ta.setStyle(tblstyle)
+        
+        return ta  
+    
+    def zap_orphans(self, trows, trip_row=38):
+        s = len(trows) % trip_row
+        if s < 2:
+            self.sample_rowids[-1] += 1
+            trows.insert(-1, [])
+            if not s:
+                self.sample_rowids[-1] += 1
+                trows.insert(-1, [])
+        
+    def _build_sample_rows(self, trows, sample):  
+        info_line = self._new_paragraph(u'<font size=9><b>{sample}</b></font>, <font size=7>{material}, {weight} mg, J={j}\xb1{jer}%, D={d}\xb1{der}, {irrad}, Lab#={l_number}</font>'.format(**sample.info))
+        trows.append(['', info_line])
+        
+        for i, a in enumerate(sample.analyses):
+            if i < 1:
+                self.zap_orphans(trows)
+                
+            trows.append(a.get_data())
+                
+        intage_label = self._new_paragraph(u'<font size=9><b>Integrated age ' + self._plusminus_sigma(2) + '</b></font>')
+        intage, intage_err = sample.get_isotopic_recombination_age()
+        platage_label = self._new_paragraph(u'<font size=9><b>Plateau ' + self._plusminus_sigma(2) + '</b></font>')
+        
+        
+        tot39 = '{:0.1f}'.format(sample.get_total39())
+        kca, _er = sample.get_kca()
+        kca = '{:0.1f}'.format(kca)
+        
+        nsteps = 'n={}'.format(sample.get_nsteps())
+        args = sample.get_plateau_steps()
+        if args:
+            ptot39 = '{:0.1f}'.format(sample.get_total39(plateau=True))
+            mswd = '{:0.2f}'.format(sample.get_mswd())
+            ps, pe, pss = args 
+            psteps = 'steps {}-{}'.format(ps, pe)
+            npsteps = 'n={}'.format(pss)
+            plat39 = '{:0.1f}'.format(sample.get_plateau_percent39())
+            age, err = map('{:0.3f}'.format, sample.get_plateau_age())
+            kca_args = sample.get_kca(plateau=True)
+            pkca = self._new_paragraph(u'<font size=9>{:0.2f}\xb1{:0.2f}</font>'.format(*kca_args))
+        else:
+            ptot39 = ''
+            mswd = ''
+            psteps = ''
+            npsteps = ''
+            plat39 = ''
+            age = ''
+            err = ''
+            kca = ''
+            pkca = ''
+        
+        self.int_plat_age_rowids.append(len(trows))
+        trows.append(['', intage_label, '', '', nsteps, '', tot39, kca, '', '', '{:0.2f}'.format(intage), '{:0.2f}'.format(intage_err)])
+        
+        n = len(sample.analyses)
+        self.zap_widows(trows, n)
+
+        trows.append(['', platage_label, '', psteps, npsteps, mswd, ptot39, pkca, '', plat39, age, err])
+        trows.append([])
+        
+    def zap_widows(self, trows, n, trip_row=38, widow_rows=2):
+        s = len(trows) % trip_row
+        if s <= widow_rows:
+            for i in range(n + widow_rows - s):
+                trows.insert(-n - 2, [])
+                self.sample_rowids[-1] += 1
+                self.int_plat_age_rowids[-1] += 1
+        
+    def _build_spectrum_table_style(self):
+        tblstyle = TableStyle([
+                               ('SPAN', (0, 0), (-1, 0)),
+                               ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                               ('FONTSIZE', (0, 0), (-1, -1), 9),
+                               ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+                               ('LINEBELOW', (0, 0), (-1, 0), 1, colors.black),
+                               ('LINEBELOW', (0, 1), (-1, 1), 1, colors.black),
+#                            ('ALIGN', (2, 0), (2, 0), 'LEFT'),
+                               ('LINEBELOW', (0, 3), (-1, 3), 1.5, colors.black),
+                               ('LINEBELOW', (0, 0), (-1, -1), 1, colors.red),
+                               
+#                               ('LINEBEFORE', (0, 0), (-1, -1), 1, colors.black),
+                               ('ALIGN', (2, 0), (-1, -1), 'CENTER')
+                              ])
+        
+        for ir in self.int_plat_age_rowids:
+            tblstyle.add('SPAN', (1, ir), (3, ir))
+            tblstyle.add('SPAN', (1, ir + 1), (2, ir + 1))
+            
+        for si in self.sample_rowids:
+            tblstyle.add('SPAN', (1, si), (-1, si))
+            
+        return tblstyle
+    
+    def _set_row_heights(self, ta):
+        ta._argH[1] = 0.0125 * inch
+        ta._argH[4] = 0.2 * inch
+        
+        for s in self.sample_ends:
+            try:
+                ta._argH[s] = 0.25 * inch
+            except IndexError:
+                break
+            
+            
+    def _set_column_widths(self, ta):
+        ta._argW[0] = 0.17 * inch
+        ta._argW[2] = 0.5 * inch
+        st = 3
+        ta._argW[st] = 0.6 * inch
+        ta._argW[st + 1] = 0.6 * inch
+        ta._argW[st + 2] = 0.6 * inch
+        ta._argW[st + 3] = 0.7 * inch
+        ta._argW[st + 4] = 0.5 * inch
+        ta._argW[st + 5] = 0.5 * inch
+        
     def create_report(self, p):
         doc = SimpleDocTemplate(p)
         _style = styles["Normal"]
-#        space = Spacer(1,0.2*inch)
+        space = Spacer(1, 0.2 * inch)
 
-        #report_text=[]
-        #report_text.append(PageBreak())
-#        for i in range(1):
-#            headertext=Paragraph(('this is a header'),style)
-#            report_text.append(headertext)
-#            report_text.append(space)
-#        
-#        from numpy import random
-#        tdata=[['L#','Sample','Age','Error(2s)']]
-#        for i in range(30):
-#            
-#            t=[1000+i,'SMP-0%i'%i,'%0.3f'%random.random(),'%0.3f'%(random.random()/100.0)]
-#            tdata.append(t)
-#        ta=Table(tdata,
-#                 colWidths=[0.5*inch,1.5*inch,1*inch,1*inch],rowHeights=[0.25*inch]*len(tdata))
-#        tblstyle=TableStyle([('BACKGROUND',(0,3),(-1,3),colors.blue),
-#                             ('TEXTCOLOR',(0,0),(-1,-1),colors.black),
-#                             ('LINEBELOW',(0,0),(-1,0),2,colors.black),
-#                             # ('LINEBEFORE',(1,1),(-1,-1),1,colors.black),
-#                             
-#                             
-#                             ('LINEBELOW',(0,1),(-1,-2),1,colors.black),
-#                              ('LINEBEFORE',(1,1),(-1,-1),1,colors.black),
-#                              ])
-#        ta.setStyle(tblstyle)
-#        ta.hAlign='LEFT'
+        report_text = []
+        
+#        report_text.append(PageBreak())
+        samples = [('AF-32', 'Groundmass Concentrate', 20.55, 0.01, 0.001, 1.0004, 0.1, 'NM-205G', '56879-01')]
+        
+        
+        parser = AutoupdateParser()
+
+        p = '/Users/Ross/Documents/Antarctica/MinnaBluff/data/test.csv'
+        p = '/Users/Ross/Documents/Antarctica/MinnaBluff/data/af50.csv'
+        p = '/Users/Ross/Documents/Antarctica/MinnaBluff/data/gm-06.csv'
+        
+        samples = parser.parse(p)
+        ta = self._build_spectrum_table(1, samples)
+        ta.hAlign = 'LEFT'
+        
+#        ta.split(PAGE_WIDTH, PAGE_HEIGHT)
 #        report_text.append(space)
 #        report_text.append(Spacer(3.0*inch,0))
-#        report_text.append(ta)    
+        report_text.append(ta)    
 #         
-        report_text = self.report_text
-        doc.build(report_text, onFirstPage=self.create_cover_page, onLaterPages=self.create_report_page)
+
+#        report_text = self.report_text
+        doc.build(report_text,
+                  #onFirstPage=self.create_cover_page, 
+                  onLaterPages=self.create_report_page)
 #        
 #        for i in range(20): 
 #            bogustext = ("This is Paragraph number %s.  " % i) *20 
@@ -149,8 +305,28 @@ class Report:
 #            Story.append(p) 
 #            Story.append(Spacer(1,0.2*inch)) 
 #        doc.build(Story, onFirstPage=self.create_cover_page, onLaterPages=self.create_report_page) 
-if __name__ == '__main__':
+def time_recursive():
     r = Report()
+#    r.recursive = True
     r.cover_text = 'This report was generated by Reporter by Jake Ross'
     r.create_report('phello2.pdf')
 
+def time_non_recursive():
+    r = Report()
+#    r.recursive = False
+    r.cover_text = 'This report was generated by Reporter by Jake Ross'
+    r.create_report('phello2.pdf')
+        
+    
+if __name__ == '__main__':
+#    from timeit import Timer
+#    t = Timer('time_recursive', 'from __main__ import time_recursive')
+#    
+#    n = 5
+#    tr = t.timeit(n)
+#    print 'time r', tr, tr / 5
+#    
+#    t = Timer('time_non_recursive', 'from __main__ import time_non_recursive')
+#    tr = t.timeit(n)
+#    print 'time nr', tr, tr / 5
+    time_non_recursive()
