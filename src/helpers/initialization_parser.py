341fbc15d9dab5a1d6bde707f87757e7742f50cc
jirhiker <jirhiker@localhost>
2012-03-04 19:03:45 -0700

    remote start of video record works only if a capture device is already opened ie video_stage_manager added processor tag to initialization_parser

--- src/helpers/initialization_parser.py
+++ src/helpers/initialization_parser.py
@@ -18,6 +18,7 @@ from xml_parser import XMLParser
 #============= standard library imports ========================
 #============= local library imports  ==========================
 
+
 class InitializationParser(XMLParser):
     def add_plugin(self, category, name):
         tree = self._tree
@@ -26,7 +27,7 @@ class InitializationParser(XMLParser):
         cat.append(self.new_element('plugin', name, enabled='false'))
         self.save()
 
-    def get_plugins(self, category=None, all=False):
+    def get_plugins(self, category=None, all=False, element=False):
         tree = self._tree.find('plugins')
 
         if category:
@@ -39,13 +40,14 @@ class InitializationParser(XMLParser):
             except AttributeError:
                 plugins = tree.getiterator(tag='plugin')
 
-        return [p.text.strip() for p in plugins if all or p.get('enabled').lower() == 'true']
+        return [p if element else p.text.strip()
+                for p in plugins if all or p.get('enabled').lower() == 'true']
 
-    def get_plugins_as_elements(self, category):
-        tree = self._tree.find('plugins')
-        cat = tree.find(category)
-        if cat is not None:
-            return cat.findall('plugin')
+#    def get_plugins_as_elements(self, category):
+#        tree = self._tree.find('plugins')
+#        cat = tree.find(category)
+#        if cat is not None:
+#            return cat.findall('plugin')
 
     def get_plugin_groups(self):
         elem = self._tree.find('plugins')
@@ -88,9 +90,8 @@ class InitializationParser(XMLParser):
         plugin.set('enabled', 'false')
         self.save()
 
-    def get_flags(self, manager, all=False, element=False):
-        return [d if element else d.text.strip()  for d in manager.findall('flag')
-                                if all or d.get('enabled').lower() == 'true']
+    def get_flags(self, manager, **kw):
+        return self._get_paramater(manager, 'flag', **kw)
 
     def get_device(self, manager, devname, plugin, element=False):
 
@@ -99,14 +100,33 @@ class InitializationParser(XMLParser):
         else:
             man = self.get_manager(manager, plugin)
 
-        dev = next((d for d in man.findall('device') if d.text.strip() == devname), None)
+        dev = next((d for d in man.findall('device')
+                    if d.text.strip() == devname), None)
         if not element and dev:
             dev = dev.text.strip()
         return dev
 
-    def get_devices(self, manager, all=False, element=False):
-        return [d if element else d.text.strip()  for d in manager.findall('device')
-                                if all or d.get('enabled').lower() == 'true']
+    def get_devices(self, manager, **kw):
+        return self._get_paramater(manager, 'device', **kw)
+
+    def get_processor(self, manager, **kw):
+        p = self._get_paramater(manager, 'processor', **kw)
+        if p:
+            return p[0]
+
+    def get_processors(self):
+#        ps = []
+#        for p in self.get_plugins('Hardware'):
+#            pp = self.get_processor(p)
+#            if pp:
+#                ps.append(pp)
+        return [pi for pi in [self.get_processor(p)
+                    for p in self.get_plugins('hardware', element=True)] if pi]
+
+    def _get_paramater(self, subtree, tag, all=False, element=False):
+        return [d if element else d.text.strip()
+                for d in subtree.findall(tag)
+                    if all or d.get('enabled').lower() == 'true']
 
     def get_managers(self, elem, all=False, element=False):
         return [m if element else m.text.strip()
@@ -149,12 +169,12 @@ class InitializationParser(XMLParser):
         p = self.get_plugin('ExtractionLine')
         return [(s.text.strip(), s.get('master_host')) for s in p.findall('system')]
 
-    def get_processors(self):
-
-        cat = self._tree.find('remotehardware')
-        pi = None
-        if cat is not None:
-            pi = cat.findall('processor')
-
-        return [pii.text.strip() for pii in (pi if pi else [])]
+#    def get_processors(self):
+#
+#        cat = self._tree.find('remotehardware')
+#        pi = None
+#        if cat is not None:
+#            pi = cat.findall('processor')
+#
+#        return [pii.text.strip() for pii in (pi if pi else [])]
 #============= EOF =============================================
