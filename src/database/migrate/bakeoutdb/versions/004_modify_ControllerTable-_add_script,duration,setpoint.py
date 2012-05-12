317116976f1f4c228f84844457596ee0385a3bb2
jirhiker <jirhiker@localhost>
2012-05-11 23:26:15 -0600

    changed bakeoutdb to sqlite

--- src/database/migrate/bakeoutdb/versions/004_modify_ControllerTable-_add_script,duration,setpoint.py
+++ src/database/migrate/bakeoutdb/versions/004_modify_ControllerTable-_add_script,duration,setpoint.py
@@ -23,20 +23,22 @@ from migrate import *
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
-    meta = MetaData(bind=migrate_engine)
-    t = Table('ControllerTable', meta, autoload=True)
-    s = Column('script', String(40))
-    sp = Column('setpoint', Float)
-    d = Column('duration', Float)
-
-    s.create(t)
-    sp.create(t)
-    d.create(t)
+#    meta = MetaData(bind=migrate_engine)
+#    t = Table('ControllerTable', meta, autoload=True)
+#    s = Column('script', String(40))
+#    sp = Column('setpoint', Float)
+#    d = Column('duration', Float)
+#
+#    s.create(t)
+#    sp.create(t)
+#    d.create(t)
+    pass
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
-    meta = MetaData(bind=migrate_engine)
-    t = Table('ControllerTable', meta, autoload=True)
-    t.c.duration.drop()
-    t.c.script.drop()
-    t.c.setpoint.drop()
+#    meta = MetaData(bind=migrate_engine)
+#    t = Table('ControllerTable', meta, autoload=True)
+#    t.c.duration.drop()
+#    t.c.script.drop()
+#    t.c.setpoint.drop()
+    pass
