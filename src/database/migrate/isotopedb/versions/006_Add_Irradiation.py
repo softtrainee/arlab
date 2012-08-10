da00ac08d45d54eb771ddcd98bedcaf157b5dc9d
jirhiker <jirhiker@localhost>
2012-08-10 11:42:21 -0600

    formatting

--- src/database/migrate/isotopedb/versions/006_Add_Irradiation.py
+++ src/database/migrate/isotopedb/versions/006_Add_Irradiation.py
@@ -1,20 +1,20 @@
 from sqlalchemy import *
 from migrate import *
 
-meta=MetaData()
-t0= Table('IrradiationPositionTable', meta,
+meta = MetaData()
+t0 = Table('IrradiationPositionTable', meta,
 	Column('id', Integer, primary_key=True),
 	Column('irradiation_id', Integer),
 	Column('irradiation_holder_id', Integer)
 )
-t1=Table("IrradiationTable", meta,
+t1 = Table("IrradiationTable", meta,
 	Column('id', Integer, primary_key=True),
 	Column('irradiation_production_id', Integer),
 	Column('irradiation_chronology_id', Integer),
 	Column('name', String(40)),
 	Column('level', String(40))
 )
-t2=Table("IrradiationProductionTable", meta,
+t2 = Table("IrradiationProductionTable", meta,
 	Column('id', Integer, primary_key=True),
 	Column('Ca3637', Float),
 	Column('Ca3637_err', Float),
@@ -22,7 +22,7 @@ t2=Table("IrradiationProductionTable", meta,
 	Column('Ca3837_err', Float),
 	Column('Ca3937', Float),
 	Column('Ca3937_err', Float),
-	
+
 	Column('K3739', Float),
 	Column('K3739_err', Float),
 	Column('K3839', Float),
@@ -30,27 +30,27 @@ t2=Table("IrradiationProductionTable", meta,
 	Column('K4039', Float),
 	Column('K4039_err', Float),
 )
-t3=Table("IrradiationChronologyTable", meta,
+t3 = Table("IrradiationChronologyTable", meta,
 	Column('id', Integer, primary_key=True),
 	Column('chronology', BLOB)
 )
-t4=Table("IrradiationHolderTable", meta,
+t4 = Table("IrradiationHolderTable", meta,
 	Column('id', Integer, primary_key=True),
 	Column('name', String(40)),
 	Column('geometry', BLOB)
 )
 
-tables=[t0,t1,t2,t3,t4]
+tables = [t0, t1, t2, t3, t4]
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
-	meta.bind=migrate_engine
+	meta.bind = migrate_engine
 	for t in tables:
 		t.create()
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
-	meta.bind=migrate_engine
+	meta.bind = migrate_engine
 	for t in tables:
 		t.drop()
