52548363f21580af6e87fa3aabe53909c6e28f43
jirhiker <jirhiker@localhost>
2012-10-12 12:20:02 -0600

    updated map view

--- src/lasers/plugins/laser_plugin.py
+++ src/lasers/plugins/laser_plugin.py
@@ -74,7 +74,6 @@ class LaserPlugin(CorePlugin):
             factory = __import__(self.klass[0], fromlist=[self.klass[1]])
             m = getattr(factory, self.klass[1])()
 
-        print m, m.name
         m.bootstrap()
         m.bind_preferences(self.id)
         return m
