1d13e7454be4484f51f7c84a406da94bbccd7e40
jirhiker <jirhiker@localhost>
2013-01-10 00:39:18 -0700

    updated pychron installer script. uses pymysql so no compilation necessary. no need for xcode, gcc, or MySQL

--- installer.py
+++ installer.py
@@ -22,37 +22,31 @@ import shutil
 #============= local library imports  ==========================
 
 '''
+    example execution
+        >>sudo python installer.py
+        
+        
     this script executes pychron installation 
     
-    1. install EPD
-    2. install XCode
-    3. install MySQL
+    install EPD
     
-    4. relink gcc
+    install uncertainties
+    install pandas 0.8
+    install statsmodels 0.4
+    install pymysql
     
-    5. install pandas 0.8
-    6. install statsmodels 0.5
-    7. install mysql-python
-    
-    8. move support files into place
+    move support files into place
 '''
 
 def main():
     if install_epd():
         return
 
-    print
-    if install_xcode():
-        return
-
-    print
-    if install_mysql():
-        return
-
-    if change_gcc_version('4.0'):
-        return
-
     root = os.getcwd()
+
+    print
+    if install_python_package(root, 'uncertainties', '1.9'):
+        return
 
     print
     if install_python_package(root, 'pandas', '0.8.1'):
@@ -63,7 +57,7 @@ def main():
         return
 
     print
-    if install_python_package(root, 'MySQL-python', '1.2.4b4'):
+    if install_python_package(root, 'PyMySQL', 'master'):
         return
 
     if move_support_files(root):
@@ -77,22 +71,23 @@ def move_support_files(root, version='processing'):
     src = os.path.join(root, name)
     shutil.copytree(src, dst)
 
-def install_python_package(root, name, version):
-    os.chdir(os.path.join(root, '{}-{}'.format(name, version)))
-    subprocess.call(['python', 'hello.py', 'install'])
-#    subprocess.call(['python', 'setup.py', 'install'])
+def install_python_package(root, name, version=None):
+    if version:
+        name = '{}-{}'.format(name, version)
+    os.chdir(os.path.join(root, name))
+    subprocess.call(['python', 'setup.py', 'install'])
 
 
-def change_gcc_version(version):
-    '''
-        change gcc version to 4.0
-    '''
-    gcc = '/usr/bin/gcc'
-    print 'changing {} symlink to use gcc version {}'.format(gcc, version)
-
-    os.remove(gcc)
-    gcc_v = '/usr/bin/gcc-{}'.format(version)
-    os.symlink(gcc_v, gcc)
+#def change_gcc_version(version):
+#    '''
+#        change gcc version to 4.0
+#    '''
+#    gcc = '/usr/bin/gcc'
+#    print 'changing {} symlink to use gcc version {}'.format(gcc, version)
+#
+#    os.remove(gcc)
+#    gcc_v = '/usr/bin/gcc-{}'.format(version)
+#    os.symlink(gcc_v, gcc)
 
 def install_epd():
     print 'open epd installer'
@@ -108,17 +103,6 @@ def user_cancel():
 
     if _input.strip() == 'n':
         return True
-
-def install_xcode():
-    print 'open xcode installer'
-    if user_cancel():
-        return True
-
-def install_mysql():
-    print 'open mysql installer'
-    if user_cancel():
-        return True
-
 
 
 if __name__ == '__main__':
