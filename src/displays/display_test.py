18eb3918180c6c295319c85355dc83708cd1335c
jirhiker <jirhiker@localhost>
2013-01-15 09:54:51 -0700

    updated display test

--- src/displays/display_test.py
+++ src/displays/display_test.py
@@ -1,5 +1,5 @@
 #===============================================================================
-# Copyright 2011 Jake Ross
+# Copyright 2012 Jake Ross
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,44 +14,40 @@
 # limitations under the License.
 #===============================================================================
 
+#============= enthought library imports =======================
+from traits.api import HasTraits, Instance, Any, Str
+from traitsui.api import View, Item, TableEditor
+from src.displays.html_display import HTMLDisplay
+from src.database.adapters.isotope_adapter import IsotopeAdapter
+from src.database.records.isotope_record import IsotopeRecord
+from src.database.isotope_analysis.analysis_display import AnalysisDisplay
+
+#============= standard library imports ========================
+#============= local library imports  ==========================
 
 
-##============= enthought library imports =======================
-#from traits.api import HasTraits, Instance
-#from traitsui.api import View, Item, Group, HGroup, VGroup
-#
-##============= standard library imports ========================
-#
-##============= local library imports  ==========================
-#from src.managers.displays.rich_text_display import RichTextDisplay, TextDisplay
-#from pyface.timer.do_later import do_later
-#from threading import Thread
-#import time
-#
-#
-#class Demo(HasTraits):
-#    display = Instance(TextDisplay)
-#    def _display_default(self):
-#        return RichTextDisplay()
-##    def _display_default(self):
-##        return TextDisplay()
-#    def start(self):
-#        t = Thread(target = self._start)
-#        t.start()
-#
-#    def _start(self):
-#        for i in range(1000):
-#            self.display.add_text('asdfsd %i' % i, color = (255, 0, 0))
-#            time.sleep(0.05)
-##============= views ===================================
-#    def traits_view(self):
-#        v = View(Item('display', show_label = False,
-#                      style = 'custom'),
-#                 resizable = True
-#                 )
-#        return v
-#if __name__ == '__main__':
-#    r = Demo()
-#    do_later(r.start)
-#    r.configure_traits()
-##============= EOF ====================================
+
+class Demo(HasTraits):
+    display = Instance(HTMLDisplay)
+    view = View(Item('display', show_label=False, style='custom'),
+                resizable=True,
+                width=600,
+                height=500
+                )
+
+    def _display_default(self):
+        db = IsotopeAdapter(host='localhost',
+                            name='isotopedb_dev',
+                            kind='mysql',
+                            username='root', password='Argon')
+        if db.connect():
+            lns = db.get_labnumber(6)
+            r = IsotopeRecord(_dbrecord=lns.analyses[-1])
+            r.load_graph()
+
+            return AnalysisDisplay(record=r)
+
+if __name__ == '__main__':
+    d = Demo()
+    d.configure_traits()
+#============= EOF =============================================
