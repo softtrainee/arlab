df489fb286a4bd80f33077e559ff433cc1f5a300
jirhiker <jirhiker@localhost>
2013-02-16 10:38:33 -0700

    lse formating

--- src/lasers/laser_managers/laser_script_executor.py
+++ src/lasers/laser_managers/laser_script_executor.py
@@ -19,132 +19,131 @@ import time
 from threading import Thread
 from src.loggable import Loggable
 import os
-from src.paths import paths        
-        
+from src.paths import paths
+
 class LaserScriptExecutor(Loggable):
-    laser_manager=Any
-    _executing=Bool(False)
-    _cancel=False
-    execute_button=Event
-    execute_label=Property(depends_on='_executing')
-    names=Enum('mask','z','attenuator','burst')
-    
+    laser_manager = Any
+    _executing = Bool(False)
+    _cancel = False
+    execute_button = Event
+    execute_label = Property(depends_on='_executing')
+    names = Enum('mask', 'z', 'attenuator', 'burst')
+
     def _get_execute_label(self):
         return 'Stop' if self._executing else 'Execute'
-    
+
     def _execute_button_fired(self):
-        n=self.names
+        n = self.names
         if n is None:
-            n='mask'
-            
-        name=os.path.join(paths.scripts_dir, 'uv_matrix_{}.yaml'.format(n))
+            n = 'mask'
+
+        name = os.path.join(paths.scripts_dir, 'uv_matrix_{}.yaml'.format(n))
         self.execute(name)
-        
+
     def execute(self, name):
         if self._executing:
-            self._cancel=True
-            self._executing=False
+            self._cancel = True
+            self._executing = False
         else:
-            self._cancel=False
-            self._executing=True
-            t=Thread(target=self._execute, args=(name,))
+            self._cancel = False
+            self._executing = True
+            t = Thread(target=self._execute, args=(name,))
             t.start()
 #        self._execute(name)
-        
+
     def _execute(self, name):
         self.info('starting LaserScript {}'.format(name))
 
-        lm=self.laser_manager
-        sm=lm.stage_manager
-        atl=lm.atl_controller
-        
-                
+        lm = self.laser_manager
+        sm = lm.stage_manager
+        atl = lm.atl_controller
+
         with open(name, 'r') as fp:
             def shot(delay=3):
                 if not self._cancel:
                     time.sleep(0.5)
                     lm.single_burst(delay=delay)
-                
-            d=yaml.load(fp.read())
-            
-            device=d['device']
-            if device=='z':
+
+            d = yaml.load(fp.read())
+
+            device = d['device']
+            if device == 'z':
                 def iteration(p):
                     sm.set_z(p, block=True)
                     shot()
-            elif device=='laser':
+            elif device == 'laser':
                 def iteration(p):
                     atl.set_nburst(p, save=False)
-                    delay=2+p/100.
+                    delay = 2 + p / 100.
                     shot(delay=delay)
             else:
-                motor=lm.get_motor(device)
+                motor = lm.get_motor(device)
                 def iteration(p):
                     motor.trait_set(data_position=p)
                     motor.block(4)
                     shot()
-                    
-            sx,sy=d['xy']
-            xstep=d['xstep']
-            ystep=d['ystep']
+
+            sx, sy = d['xy']
+            xstep = d['xstep']
+            ystep = d['ystep']
 #            ny=d['y_nsteps']
 #            nx=d['x_nsteps']
-            ncols=d['ncols']
-            ms=d['start']
-            me=d['stop']
-            mstep=d['step']
-            sign=1
-            if me<ms:
-                sign=-1
-                
-            n=(abs(ms-me)+1)/mstep
-            
-            nx=ncols
-            ny=int(n/int(ncols)+1)
-            
-            v=d['velocity']
+            ncols = d['ncols']
+            ms = d['start']
+            me = d['stop']
+            mstep = d['step']
+            sign = 1
+            if me < ms:
+                sign = -1
+
+            n = (abs(ms - me) + 1) / mstep
+
+            nx = ncols
+            ny = int(n / int(ncols) + 1)
+
+            v = d['velocity']
 #            atl.set_nburst(nb)
-            dp=ms
+            dp = ms
             for r in range(ny):
                 if self._cancel:
                     break
-                
+
                 for c in range(nx):
                     if self._cancel:
                         break
-                    
-                    dp=ms+(r*nx+c)*mstep                    
-                    
-                    if sign ==1:
-                        if dp>me:
+
+                    dp = ms + (r * nx + c) * mstep
+
+                    if sign == 1:
+                        if dp > me:
                             break
                     else:
-                        if dp<me:
+                        if dp < me:
                             break
-                        
-                    x,y=sx+c*xstep, sy+r*ystep
-                    
-                    sm.linear_move(x,y, velocity=v,block=True)
+
+                    x, y = sx + c * xstep, sy + r * ystep
+
+                    sm.linear_move(x, y, velocity=v, block=True)
                     if self._cancel:
                         break
-                    
+
                     iteration(dp)
                     if self._cancel:
                         break
-                    
-                if sign ==1:
-                    if dp>me:
+
+                if sign == 1:
+                    if dp > me:
                         break
                 else:
-                    if dp<me:
+                    if dp < me:
                         break
             else:
-                self.info('LaserScript truncated at row={}, col={}'.format(r,c))
-                
-            self._executing=False
+                self.info('LaserScript truncated at row={}, col={}'.format(r, c))
+
+            self._executing = False
             self.info('LaserScript finished'.format(name))
-            
+
 if __name__ == '__main__':
-    lm=LaserScriptExecutor()
-    name='/Users/uv/Pychrondata_uv/scripts/uv_laser.yaml'
+    lm = LaserScriptExecutor()
+    name = '/Users/uv/Pychrondata_uv/scripts/uv_laser.yaml'
     lm.execute(name)
