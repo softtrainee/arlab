ca921bac107024804f5019f62da7b3483c5d4425
jirhiker <jirhiker@localhost>
2013-05-12 16:17:45 -0600

    moved zobs

--- src/pyscripts/wait_dialog.py
+++ src/pyscripts/wait_dialog.py
@@ -15,13 +15,20 @@
 #===============================================================================
 
 
-
+from traits.etsconfig.etsconfig import ETSConfig
+from src.helpers.timer import Timer
+ETSConfig.toolkit = 'qt4'
+from src.ui.qt.gui import invoke_in_main_thread
 #============= enthought library imports =======================
 from traits.api import Float, Property, Bool, Str
 from traitsui.api import View, Item, RangeEditor
-from pyface.timer.timer import Timer
+# from pyface.timer.timer import Timer
 from traitsui.menu import Action
 from src.viewable import ViewableHandler, Viewable
+from threading import Event
+import time
+from src.helpers import logger_setup
+from src.helpers.logger_setup import logging_setup
 
 #============= standard library imports ========================
 
@@ -30,12 +37,12 @@ class WDHandler(ViewableHandler):
 #    def init(self, info):
 #        info.object.ui = info.ui
 #
-#    def closed(self, info, is_ok):
-#        info.object.closed(is_ok)
-#        return True
+    def closed(self, info, is_ok):
+        info.object.closed(is_ok)
+        return True
 #    def close(self, info, isok):
 #        print 'close', isok
-#        
+#
 #        return True
 
     def _continue(self, info):
@@ -65,18 +72,22 @@ class WaitDialog(Viewable):
         if self.auto_start:
 #            if self.condition is not None:
 #                self.condition.acquire()
-            if self.end_evt is not None:
-                self.end_evt.clear()
-            self.timer = Timer(1000, self._update_time)
+#            if self.end_evt is not None:
+#                self.end_evt.clear()
+#            self.timer = Timer(1000, self._update_time)
+            self.start(evt=self.end_evt)
 
     def start(self, evt=None):
-        evt.clear()
-        self.end_evt = evt
+        if evt:
+            evt.clear()
+            self.end_evt = evt
 #        self.condition = condition
 #        if self.condition is not None:
 #            self.condition.acquire()
-
-        self.timer = Timer(1000, self._update_time)
+        self.timer = Timer(1000, self._update_time,
+                           delay=1000)
+#        self.timer = Timer(1000, self._update_time)
+#        print self.timer
         self._continued = False
 
     def stop(self):
@@ -106,25 +117,10 @@ class WaitDialog(Viewable):
             self.debug('disposing {}'.format(self))
             self.disposed = True
 
-    def close(self, isok):
-        super(WaitDialog, self).close(isok)
-        self._canceled = not isok
-
-        if self.timer is not None:
-            self.timer.Stop()
-        if self.end_evt is not None:
-            self.end_evt.set()
+    def closed(self, isok):
+        self._end()
         return True
 
-#    def closed(self, isok):
-#        self.debug('closed {}'.format(isok))
-##        self._current_time = -1
-##        self._end()
-##        if isok is not None:
-#
-#        self._end()
-#        return True
-#
     def _continue(self):
         self._canceled = False
         self._continued = True
@@ -155,5 +151,61 @@ class WaitDialog(Viewable):
 #                    buttons = ['Cancel'],
 #                    handler = WDHandler
                     )
+
+from traits.api import HasTraits, Button
+class Demo(HasTraits):
+    test = Button
+    def traits_view(self):
+        v = View('test')
+        return v
+
+    def _test_fired(self):
+        from src.ui.thread import Thread
+        t = Thread(target=self._wait)
+        t.start()
+        self._t = t
+
+    def _wait(self):
+        timeout = 5
+        message = 'foo'
+        evt = Event()
+
+        wd = WaitDialog(wtime=timeout,
+                            end_evt=evt,
+    #                        parent=self,
+    #                        title='{} - Wait'.format(self.logger_name),
+                            message='Waiting for {:0.1f}  {}'.format(timeout, message)
+                            )
+        invoke_in_main_thread(wd.edit_traits)
+        evt.wait(timeout=timeout + 0.25)
+#        wd.edit_traits(kind='livemodal')
+
+#
+#        from threading import Thread
+#        def wait():
+#            st = time.time()
+#            t = 0
+#            while t < (timeout + 0.25) and not evt.is_set():
+#                time.sleep(0.5)
+#                t = time.time() - st
+# #
+#
+#        t = Thread(target=wait)
+#        t.start()
+#        t.join()
+        print 'wati comi'
+
+#        st = time.time()
+#        t = 0
+#        while t < (timeout + 0.25) and not evt.is_set():
+#            time.sleep(0.5)
+#            t = time.time() - st
+#
+# #        evt.wait(timeout=timeout + 0.25)
+#        wd.stop()
+
+if __name__ == '__main__':
+    logging_setup('foo')
+    Demo().configure_traits()
 #============= views ===================================
 #============= EOF ====================================
