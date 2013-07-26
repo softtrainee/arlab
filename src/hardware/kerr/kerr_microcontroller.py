0e78c27fd908d1315f8449305c2c895acff36f34
jirhiker <jirhiker@localhost>
2013-07-26 14:51:02 -0600

    kerr updates

--- src/hardware/kerr/kerr_microcontroller.py
+++ src/hardware/kerr/kerr_microcontroller.py
@@ -35,11 +35,35 @@ class KerrMicrocontroller(KerrDevice):
         self.info('init microcontroller')
         self.parent.tell('0' * 40, is_hex=True)
 
+        cmd = self._build_command('FF', '0F')
+        self.tell(cmd)
+
         addr = self.address
         commands = [(addr, '2101FF', 100, 'setting module 1 address'),
                     (addr, '2102FF', 100, 'setting module 2 address'),
                     (addr, '2103FF', 100, 'setting module 3 address'),
 
                   ]
-        self._execute_hex_commands(commands, tell=True)
+        self._execute_hex_commands(commands, tell=True, delay=100)
+
+
+        #verify number of modules found
+#        commands = [
+#                    (addr, '010E', 50, 'no op'),
+#                    (addr, '020E', 50, 'no op')
+#                    ]
+#        self._execute_hex_commands(commands)
+
+#        addr = self.address
+        cmd = '0E'
+        for addr in ('01', '02'):
+            c = self._build_command(addr, cmd)
+#            print addr, c
+            status_byte = self.ask(c, is_hex=True,
+                                    delay=100,
+                                    nbytes=2,
+                                    info='get defined status',
+                                    verbose=True)
+            print addr, cmd, status_byte
+
         return True
