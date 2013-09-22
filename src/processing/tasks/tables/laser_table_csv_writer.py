d371d8fa1b42cfdfcd45b7a6018f539787944a9e
jirhiker <jirhiker@localhost>
2013-09-21 18:19:20 -0600

    added laser table csv writer

--- src/processing/tasks/tables/laser_table_csv_writer.py
+++ src/processing/tasks/tables/laser_table_csv_writer.py
@@ -13,19 +13,103 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #===============================================================================
+# from traits.etsconfig.etsconfig import ETSConfig
+# ETSConfig.toolkit = 'qt4'
 
 #============= enthought library imports =======================
-from traits.api import HasTraits
-from traitsui.api import View, Item
-from src.loggable import Loggable
+from traits.api import HasTraits, Any, Str, Callable, Either, List
+
+import csv
 #============= standard library imports ========================
 #============= local library imports  ==========================
+from src.processing.tasks.tables.laser_table_text_writer import LaserTableTextWriter
 
-class LaserTableCSVWriter(Loggable):
-    def build(self, p, ans, means, title):
 
+class CSVCell(HasTraits):
+    txt = Any
+    fmt = Either(Str, Callable)
+    def render(self):
+        txt = self.txt
+        if txt is None:
+            return ''
+
+        fmt = self.fmt
+        if fmt is None:
+            fmt = str
+
+
+        if isinstance(fmt, str):
+            return fmt.format(txt)
+        else:
+            return fmt(txt)
+
+class CSVRow(HasTraits):
+    cells = List
+    def render(self, writer):
+        rows = [ci.render() for ci in self.cells]
+        writer.writerow(rows)
+
+    def write(self, c, v):
+        while c >= len(self.cells) or not len(self.cells):
+            self.cells.append(CSVCell())
+
+        nc = self.cells[c]
+        nc.txt = v
+
+
+class CSVSheet(HasTraits):
+    rows = List
+    name = Str
+    def render(self, writer):
+        writer.writerow([self.name, ])
+        for ri in self.rows:
+            ri.render(writer)
+
+        writer.writerow([])
+
+    def write(self, r, c, v, *args, **kw):
+        while r >= len(self.rows) or not len(self.rows):
+            self.rows.append(CSVRow())
+
+        nr = self.rows[r]
+        nr.write(c, v)
+
+    def merge(self, *args, **kw):
         pass
 
 
+class CSVWorkbook(HasTraits):
+    '''
+     emulates a xlwt Workbook
+    '''
+    sheets = List
+
+    def add_sheet(self, name):
+        sh = CSVSheet(name=name)
+        self.sheets.append(sh)
+        return sh
+
+    def save(self, p, **kw):
+        with open(p, 'w') as fp:
+            writer = csv.writer(fp)
+
+            for sh in self.sheets:
+                sh.render(writer)
+
+
+class LaserTableCSVWriter(LaserTableTextWriter):
+    def _new_workbook(self):
+        return CSVWorkbook()
+
+
+# if __name__ == '__main__':
+#     l = LaserTableCSVWriter()
+#     from src.processing.analyses.analysis import DBAnalysis
+#     ans = [DBAnalysis(sample='foo', labnumber='11111') for i in range(5)]
+#     ans.extend([DBAnalysis(sample='bar',
+#                            labnumber='22222'
+#                            ) for i in range(5)])
+#     p = '/Users/ross/Sandbox/aaaatable.csv'
+#     l.build(p, ans, [], 'foo')
 
 #============= EOF =============================================
