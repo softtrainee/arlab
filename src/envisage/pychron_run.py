6907f081c69d3f5a39d696e10f2e35c9ad7445c6
ross <ross@Jakes-MacBook-Pro.local>
2013-09-29 16:26:19 -0600

    changed preferences to a list str editor and a selected item

--- src/envisage/pychron_run.py
+++ src/envisage/pychron_run.py
@@ -21,6 +21,7 @@ from envisage.ui.tasks.tasks_plugin import TasksPlugin
 #============= standard library imports ========================
 #============= local library imports  ==========================
 from src.displays.gdisplays import gTraceDisplay
+from src.envisage.tasks.tasks_plugin import myTasksPlugin
 from src.helpers.logger_setup import new_logger
 from src.logger.tasks.logger_plugin import LoggerPlugin
 from src.initialization_parser import InitializationParser
@@ -29,40 +30,41 @@ from src.initialization_parser import InitializationParser
 logger = new_logger('launcher')
 
 PACKAGE_DICT = dict(
-#                   DatabasePlugin='src.database.plugins.database_plugin',
-                   ExperimentPlugin='src.experiment.tasks.experiment_plugin',
-                   LoadingPlugin='src.loading.loading_plugin',
-                   ExtractionLinePlugin='src.extraction_line.tasks.extraction_line_plugin',
-                   VideoPlugin='src.image.tasks.video_plugin',
-#                   CanvasDesignerPlugin='src.canvas.plugins.canvas_designer_plugin',
-#                   MDDModelerPlugin='src.modeling.plugins.mdd_modeler_plugin',
+    #                   DatabasePlugin='src.database.plugins.database_plugin',
+    ExperimentPlugin='src.experiment.tasks.experiment_plugin',
+    LoadingPlugin='src.loading.loading_plugin',
+    ExtractionLinePlugin='src.extraction_line.tasks.extraction_line_plugin',
+    VideoPlugin='src.image.tasks.video_plugin',
+    #                   CanvasDesignerPlugin='src.canvas.plugins.canvas_designer_plugin',
+    #                   MDDModelerPlugin='src.modeling.plugins.mdd_modeler_plugin',
 
-#                   SVNPlugin='src.svn.plugins.svn_plugin',
+    #                   SVNPlugin='src.svn.plugins.svn_plugin',
 
-                   FusionsDiodePlugin='src.lasers.tasks.plugins.diode',
-                   FusionsCO2Plugin='src.lasers.tasks.plugins.co2',
-                   FusionsUVPlugin='src.lasers.tasks.plugins.uv',
-                   CoreLaserPlugin='src.lasers.tasks.plugins.laser_plugin',
-#                   FusionsDiodePlugin='src.lasers.plugins.fusions.diode.plugin',
-#                   FusionsCO2Plugin='src.lasers.plugins.fusions.co2.plugin',
-#                   FusionsUVPlugin='src.lasers.plugins.fusions.uv.plugin',
+    FusionsDiodePlugin='src.lasers.tasks.plugins.diode',
+    FusionsCO2Plugin='src.lasers.tasks.plugins.co2',
+    FusionsUVPlugin='src.lasers.tasks.plugins.uv',
+    CoreLaserPlugin='src.lasers.tasks.plugins.laser_plugin',
+    #                   FusionsDiodePlugin='src.lasers.plugins.fusions.diode.plugin',
+    #                   FusionsCO2Plugin='src.lasers.plugins.fusions.co2.plugin',
+    #                   FusionsUVPlugin='src.lasers.plugins.fusions.uv.plugin',
 
-#                   SynradCO2Plugin='src.lasers.plugins.synrad_co2_plugin',
+    #                   SynradCO2Plugin='src.lasers.plugins.synrad_co2_plugin',
 
-                   SpectrometerPlugin='src.spectrometer.tasks.spectrometer_plugin',
+    SpectrometerPlugin='src.spectrometer.tasks.spectrometer_plugin',
 
-#                   GraphPlugin='src.graph.plugins.graph_plugin',
+    #                   GraphPlugin='src.graph.plugins.graph_plugin',
 
-#                   TwitterPlugin='src.social.plugins.twitter_plugin',
-#                   EmailPlugin='src.social.plugins.email_plugin',
+    #                   TwitterPlugin='src.social.plugins.twitter_plugin',
+    #                   EmailPlugin='src.social.plugins.email_plugin',
 
-                   ProcessingPlugin='src.processing.tasks.processing_plugin',
+    ProcessingPlugin='src.processing.tasks.processing_plugin',
 
-                   MediaServerPlugin='src.media_server.tasks.media_server_plugin',
-                   PyScriptPlugin='src.pyscripts.tasks.pyscript_plugin',
-                   DatabasePlugin='src.database.tasks.database_plugin'
+    MediaServerPlugin='src.media_server.tasks.media_server_plugin',
+    PyScriptPlugin='src.pyscripts.tasks.pyscript_plugin',
+    DatabasePlugin='src.database.tasks.database_plugin'
 
-                 )
+)
+
 
 def get_module_name(klass):
     words = []
@@ -76,15 +78,18 @@ def get_module_name(klass):
 
     return '_'.join(words)
 
+
 def get_hardware_plugins():
     ip = InitializationParser()
 
     ps = []
     if 'hardware' in ip.get_categories():
         from src.hardware.tasks.hardware_plugin import HardwarePlugin
+
         if ip.get_plugins('hardware'):
             ps = [HardwarePlugin(), ]
     return ps
+
 
 def get_klass(package, name):
     try:
@@ -93,12 +98,14 @@ def get_klass(package, name):
 
     except ImportError, e:
         import traceback
+
         traceback.print_exc()
         klass = None
         logger.warning('****** {} could not be imported {} ******'.format(name, e),
-                       extra={'threadName_':'Launcher'}
-                       )
+                       extra={'threadName_': 'Launcher'}
+        )
     return klass
+
 
 def get_plugin(pname):
     gdict = globals()
@@ -114,8 +121,8 @@ def get_plugin(pname):
     elif not pname.endswith('UIPlugin'):
         # dont warn if uiplugin not available
         logger.warning('***** {} not available ******'.format(pname),
-                       extra={'threadName_':'Launcher'}
-                       )
+                       extra={'threadName_': 'Launcher'}
+        )
 
     if klass is not None:
         plugin = klass()
@@ -125,17 +132,17 @@ def get_plugin(pname):
                 return plugin
             else:
                 logger.warning('****** {} not available {}******'.format(klass, check),
-                               extra={'threadName_':'Launcher'})
+                               extra={'threadName_': 'Launcher'})
         else:
             logger.warning('***** Invalid {} needs to be a subclass of Plugin ******'.format(klass),
-                           extra={'threadName_':'Launcher'})
+                           extra={'threadName_': 'Launcher'})
 
 
 def get_user_plugins():
-    '''
-    '''
+    """
+    """
     # append plugins dir to the sys path
-#    sys.path.append(plugins_dir)
+    #    sys.path.append(plugins_dir)
 
     plugins = []
     ps = InitializationParser().get_plugins()
@@ -155,16 +162,17 @@ def get_user_plugins():
 
     return plugins
 
+
 def app_factory(klass):
-    '''
-        assemble the plugins 
+    """
+        assemble the plugins
         return a Pychron WorkbenchApplication
-    '''
+    """
     plugins = [
-               CorePlugin(),
-               TasksPlugin(),
-               LoggerPlugin()
-               ]
+        CorePlugin(),
+        myTasksPlugin(),
+        LoggerPlugin()
+    ]
 
     plugins += get_hardware_plugins()
     plugins += get_user_plugins()
@@ -172,15 +180,17 @@ def app_factory(klass):
     app = klass(plugins=plugins)
     return app
 
+
 def check_dependencies():
     '''
         check the dependencies and 
     '''
     from pyface.api import warning
+
     try:
         mod = __import__('uncertainties',
                          fromlist=['__version__']
-                         )
+        )
         __version__ = mod.__version__
     except ImportError:
         warning(None, 'Install "{}" package. required version>={} '.format('uncertainties', '2.1'))
@@ -192,7 +202,7 @@ def check_dependencies():
         warning(None, 'Update "{}" package. your version={}. required version>={} '.format('uncertainties',
                                                                                            __version__,
                                                                                            '2.1'
-                                                                                           ))
+        ))
         return
 
     return True
@@ -209,7 +219,7 @@ def launch(klass):
 
     try:
         app.run()
-        logger.info('Quitting {}'.format(app.name), extra={'threadName_':'Launcher'})
+        logger.info('Quitting {}'.format(app.name), extra={'threadName_': 'Launcher'})
     except Exception:
         logger.exception('Launching error')
         import traceback
@@ -222,7 +232,6 @@ def launch(klass):
         app.exit()
 
     return
-
 
 
 #============= EOF ====================================
